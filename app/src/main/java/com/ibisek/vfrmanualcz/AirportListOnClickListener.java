ebf239bc9fcabf7b2f39273579f22515e3129ccf
ibisek <ibisek@protonmail.com>
2020-08-03 18:30:55 +0200

    add selected item from the Nearest list to the main (LRU) list view

--- app/src/main/java/com/ibisek/vfrmanualcz/AirportListOnClickListener.java
+++ app/src/main/java/com/ibisek/vfrmanualcz/AirportListOnClickListener.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
+
+import com.ibisek.vfrmanualcz.config.Configuration;
 import com.ibisek.vfrmanualcz.data.AirportRecord;
 
-public class AirportListOnClickListener implements AdapterView.OnItemClickListener {
+import java.util.List;
+
+public class AirportListOnClickListener implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private Activity activity;
     private AirportListItemAdapter listItemAdapter;
@@ -21,7 +25,14 @@ public class AirportListOnClickListener implements AdapterView.OnItemClickListen
         System.out.println("LIST ITEM CLICKED:" + position);
         AirportRecord rec = listItemAdapter.values.get(position);
 
-        if(rec.contacts.size() > 0) {    // UL fields don't have any additional info.. (!YET!) //TODO WILL HAVE SOON!!
+        // add clicked item to top of the main list while in NearestActivity:
+        if (listItemAdapter.getContext() instanceof NearestActivity) {
+            List<AirportRecord> records = Configuration.getInstance(listItemAdapter.getContext()).getLastRecentlyUsedSearchResults();
+            addValueToBeginningOfList(records, rec);
+            Configuration.getInstance(listItemAdapter.getContext()).saveLastRecentlyUsedSearchResults(records);
+        }
+
+        if(rec.contacts.size() > 0) {    // UL fields don't have any additional info.. (!YET!) //TODO WILL HAVE SOON!! (says RLP.cz)
             //Intent mapIntent = new Intent(MainActivity.this, MapViewActivity.class);
             Intent mapIntent = new Intent(activity, SwipeActivity.class);
             mapIntent.putExtra("airportCode", rec.code);
@@ -29,4 +40,32 @@ public class AirportListOnClickListener implements AdapterView.OnItemClickListen
             activity.startActivity(mapIntent);
         }
     }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        System.out.println("LIST ITEM <LONG> CLICKED:" + position);
+        AirportRecord rec = listItemAdapter.values.get(position);
+
+        if (listItemAdapter.getContext() instanceof MainActivity) { // cannot be removed in nearest landables list
+            listItemAdapter.values.remove(position);
+            listItemAdapter.notifyDataSetChanged();
+        }
+
+        // TODO send freq to radio instead (if KRT2BT is in list of BT devices)
+
+        return true;
+    }
+
+    private void addValueToBeginningOfList(List<AirportRecord> records, AirportRecord record) {
+        // if already present.. move it to the beginning:
+        if (records.contains(record)) {
+            if (records.indexOf(record) > 0) {
+                records.remove(record);
+                records.add(0, record);
+            }
+        } else { // add to the beginning:
+            records.add(0, record);
+        }
+    }
+
 }
